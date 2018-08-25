e58fcdae061bdfa11b467f7e350c49bb36efbab2
Abemarkar23 <abemarkar23@gmail.com>
2018-08-24 18:28:05 -0700

    Added constraints to labels, button, etc.

--- Rocket Game/Rocket Game/StartViewController.swift
+++ Rocket Game/Rocket Game/StartViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Firebase
+import GoogleMobileAds
 
 enum Difficulty {
     case hard
@@ -24,7 +26,7 @@ let wndow = UIApplication.shared.keyWindow
 var topSafeArea: CGFloat = 0
 var bottomSafeArea: CGFloat = 0
 
-class StartViewController: UIViewController {
+class StartViewController: UIViewController, GADBannerViewDelegate {
     
     override func viewDidLayoutSubviews() {
         print(view.safeAreaInsets.bottom)
@@ -34,13 +36,23 @@ class StartViewController: UIViewController {
     
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
         setLabels()
         StartButton.applyAdditionalButtonDesign()
         if isDarkMode == true {
             view.backgroundColor = .black
         }
+        
+        let request = GADRequest()
+        request.testDevices = [kGADSimulatorID]
+        
+//        myBanner.adUnitID = "ca-app-pub-6099571360203191/5211532454"
+        myBanner.adUnitID = "ca-app-pub-6099571360203191/9767206208"
+
+        myBanner.rootViewController = self
+        myBanner.delegate = self
+        myBanner.load(request)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,6 +64,8 @@ class StartViewController: UIViewController {
     @IBOutlet weak var PreviousScoreLabel: UILabel!
     
     @IBOutlet weak var StartButton: UIButton!
+    
+    @IBOutlet weak var myBanner: GADBannerView!
     
     @IBAction func SelectDifficulty(_ sender: UIButton) {
         gameDifficulty = Difficulty.easy
